4ba62b09416fe433645c646b675d22eaef049cdb
AishwaryaXworkz <aishwaryabm.xworkz@gmail.com>
2023-06-06 10:50:00 +0530

    adding sql files

---
+++ Day1.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE march_27th;
+use march_27th;
+CREATE DATABASE market;
+use market;
+CREATE DATABASE hotels;
+use hotels;
+CREATE DATABASE instagram;
+use instagram;
+CREATE DATABASE whatsapp;
+use whatsapp;
+CREATE DATABASE plants;
+use plants;
+
+
